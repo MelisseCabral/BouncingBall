88150d7cc6276331e20dd29a54e82cb2e8b48a44
Melisse Cabral <melissecabral@gmail.com>
2020-03-06 00:10:33 -0300

    add bouncing, controling of paddles with the mouse

---
+++ assets/js/main.js
@@ -0,0 +1,114 @@
+var canvas;
+var canvasContext;
+var framesPerSecond = 30;
+
+let ball = {
+    x: 50,
+    speedX: 7,
+    y: 50,
+    speedY: 3,
+}
+
+const PADDLE_HEIGHT = 100;
+var paddle1Y = 250;
+var paddle2Y = 250;
+
+window.onload = function(){
+    
+    canvas = document.getElementById("gameCanvas");
+    canvasContext = canvas.getContext('2d');
+
+    canvas.addEventListener('mousemove', e => {
+        var mousePos = calculateMousePosition(e);
+        if(mousePos.x > 400){
+            paddle2Y = mousePos.y - PADDLE_HEIGHT/2;
+        } else {
+            paddle1Y = mousePos.y - PADDLE_HEIGHT/2;
+        }
+    });
+
+    draw();
+    this.setInterval(this.draw, 1000/this.framesPerSecond);
+
+}
+
+function draw () {
+    increaseBallPosition();
+
+    colorRect(0, 0, canvas.width, canvas.height, 'black');
+
+    colorRect(5, paddle1Y, 10, PADDLE_HEIGHT, 'white');
+    colorRect(785, paddle2Y, 10, PADDLE_HEIGHT, 'white');
+    colorArc(ball.x, ball.y, 10, 0, 'green')
+    
+}
+
+function colorRect(leftX, topY, width, height, drawColor){
+    canvasContext.fillStyle =  drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
+}
+function colorArc(leftX, topY, radius){
+    canvasContext.beginPath();
+    canvasContext.arc(leftX, topY, radius, 0, Math.PI*2, true);
+    canvasContext.fill();
+}
+
+function increaseBallPosition(x, y){
+    let limitX = canvas.width - 10;
+    let limitY = canvas.height - 10;
+
+    ball.x = ball.x + ball.speedX;
+    ball.y = ball.y + ball.speedY;
+    
+    if(ball.x <= 15){
+        if(ball.y > paddle1Y && ball.y < paddle1Y + PADDLE_HEIGHT){
+            ball.speedX = - ball.speedX
+            ball.speedY = - ball.speedY
+        } 
+         else {
+            resetBall();
+        } 
+    }
+    if(ball.x >= 785){
+        if(ball.y > paddle2Y && ball.y < paddle2Y + PADDLE_HEIGHT){
+            ball.speedX = - ball.speedX
+            ball.speedY = - ball.speedY
+        } 
+         else {
+            resetBall();
+        } 
+    }
+    if(ball.y <= 0){
+        if(ball.y > paddle1Y && ball.y < paddle1Y + PADDLE_HEIGHT){
+            ball.speedX = - ball.speedX
+            
+        } 
+         else {
+            resetBall();
+        } 
+    }
+    if(ball.y >= 600){
+        if(ball.y > paddle2Y && ball.y < paddle2Y + PADDLE_HEIGHT){
+            ball.speedX = - ball.speedX
+        } 
+         else {
+            resetBall();
+        } 
+    }
+    
+}
+
+function calculateMousePosition(evt){
+    let rect = canvas.getBoundingClientRect();
+    let root = document.documentElement;
+
+    return {
+        x: evt.clientX - rect.left - root.scrollLeft,
+        y: evt.clientY - rect.top - root.scrollTop
+    }
+}
+
+function resetBall(){
+    ball.x = canvas.width/2;
+    ball.y = canvas.height/2;
+}
